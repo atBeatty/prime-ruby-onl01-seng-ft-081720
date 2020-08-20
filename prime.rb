b2573647431d6a30a5bdb2f712f8727fc5888ac5
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-20 18:02:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,17 +3,14 @@ def prime?(int)
   if int == 2 || int == 3
     true
   elsif int % 2 == 0 || int <= 0 || int == 1
-    return false
+    false
+  end
 
-
-  else
-
-    counter = 2
-    while counter < int
-      counter += 1
-      if int % counter == 0
-        false
-      end
+  counter = 2
+  while counter < int
+    counter += 1
+    if int % counter == 0
+      false
     end
   end
 
